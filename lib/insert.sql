edc71184baef549d77ceb05cf2b81d99993b9280
Nestor <nestor.venegas95@gmail.com>
2021-03-07 15:14:43 -0600

    commit tree 55f7b584b44cdfbd2671401ac35f043328b3ac7e

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, "xela", "coffee", 10000, "febuary 22", "april 1"),
+(2, "twin peaks", "cooking", 50000, "march 3", "march 30"),
+(3, "best buy", "bests", 1200, "march 1", "march 2"),
+(4, "dummy thicc cheese", "cooking", 1000000, "march 1", "march 31"),
+(5, "crapsbuck", "coffee", 10000, "febuary 21", "april 2"),
+(6, "magical carp", "sushi", 5000, "febuary 28", "march 3"),
+(7, "zombie land 3", "movie", 999999999, "january 1", "december 22"),
+(8, "makin mashed potatoes", "cooking", 50000, "may 5", "october 11"),
+(9, "pokeman 12", "movie", 100000, "august 8", "october 8"),
+(10, "luis's day off", "movie", 100, "june 1", "june 8")
+
+INSERT INTO users (id, name, age) VALUES
+
+
+INSERT INTO pledges (id, user_id, project_id) VALUES
